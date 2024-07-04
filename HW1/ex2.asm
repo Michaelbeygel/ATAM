fc69b1cc5a2f21c891f92187a7aa506557bd6ce8
Aviv Avital <avivavital111@gmail.com>
2024-07-04 23:02:06 +0300

    code is compiling

--- HW1/ex2.asm
+++ HW1/ex2.asm
@@ -8,6 +8,7 @@ _start:
 
     # Initialize pointers and load values
     leaq data(%rip), %rdi   # Pointer to data
+    xor %r8, %r8            # Initialize counter
     movq %rsi, %rcx         # Size of data in bytes
     decq %rcx               # Ignore the terminator null
     movq %rcx, %rbx         # Save original size
@@ -18,18 +19,51 @@ simple_string_check:
     movb (%rdi), %al       # Load byte from data
     testb %al, %al
     je not_scientific_string # If byte is null, then it is not a string (scientific or simple)
+    cmpb $',', %al
+    je continue_simple_string # If byte is comma, continue looping
+    cmpb $'.', %al
+    je continue_simple_string # If byte is dot, continue looping
+    cmpb $'?', %al
+    je continue_simple_string # If byte is question mark, continue looping
+    cmpb $'!', %al
+    je continue_simple_string # If byte is exclamation mark, continue looping
     cmpb $' ', %al
+    je continue_simple_string # If byte is space, continue looping    
+    jmp check_numbers
+
+check_numbers:
+    cmpb $'0', %al
+    jl check_lowercase
+    cmpb $'9', %al
+    jg check_lowercase # If byte is under 0, continue looping
+    jmp continue_simple_string
+
+check_lowercase:
+    cmpb $'a', %al
+    jl check_uppercase
+    cmpb $'z', %al
+    jg check_uppercase
+    jmp continue_simple_string
+
+check_uppercase:
+    cmpb $'A', %al
     jl not_simple_string
-    cmpb $'~', %al
+    cmpb $'Z', %al
     jg not_simple_string
+    jmp continue_simple_string
+
+continue_simple_string:
     incq %rdi
-    cmpq %rdi, %rbx         # Compare current pointer with original size
+    incq %r8
+    cmpq %r8, %rbx         # Compare current pointer with original size
     je set_type_simple
     jmp simple_string_check # Otherwise, continue looping
 
 not_simple_string:
     # Scientific string test
     leaq data(%rip), %rdi
+    xor %r8, %r8            # Initialize counter
+
 scientific_string_check:
     movb (%rdi), %al       # Load byte from data
     testb %al, %al
@@ -39,7 +73,8 @@ scientific_string_check:
     cmpb $126, %al
     jg not_scientific_string
     incq %rdi
-    cmpq %rdi, %rbx         # Compare current pointer with original size
+    incq %r8
+    cmpq %r8, %rbx         # Compare current pointer with original size
     je set_type_scientific
     jmp scientific_string_check # Otherwise, continue looping
 
@@ -47,7 +82,7 @@ not_scientific_string:
     # Check divisibility by 8, and non-nullity test of all 8 consecutive bytes
     leaq data(%rip), %rdi
     movq %rbx, %rcx
-    testq %rbx, $7
+    testq $7, %rbx
     jnz done    # If not divisible by 8, set type to 4 (default)
 check_blocks:
     movq (%rdi), %rax       # Load 8 bytes from data
@@ -73,4 +108,3 @@ set_type_divisible:
 
 done:
     # Exit the program
-
