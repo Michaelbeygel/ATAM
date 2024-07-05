b76ed1f20dddfac9aec7de186ba1e531a3a5eae7
Michaelbeygel <michaelbeygel@gmail.com>
2024-07-05 14:41:54 +0300

    ex4 done

--- HW1/ex4.asm
+++ HW1/ex4.asm
@@ -5,7 +5,7 @@ _start:
 
     xor %r10, %r10   # counter for number of "good nodes"
 
-    mov $0, %r12   # counter for number of nodes to check on
+    xor %r12, %r12   # counter for number of nodes to check on
 
 
     lea nodes(%rip), %rbp    # Load address of nodes into rbp
@@ -47,22 +47,28 @@ found_last_node:
     # status before loop on all the nodes in the list:
     # r8 = first node of the list
     # r9 = last node of the list
-sort_check_for_all_nodes: # loop for each of the 3 nodes
+all_nodes_loop: # loop for each of the 3 nodes
     cmp $3, %r12 # if we check all 3 nodes
-    jge Done
+    je set_result
     mov (%rbp, %r12, 8), %rsi # rsi = the next node in the given list
     inc %r12
+    mov %rsi, %rdi # store the current node
 
 
 check_left_side:
     xor %r11, %r11 # check if left side is checked
     mov %r8, %rax 
     mov (%rsi), %rbx
-    jmp check_sorted
+    jmp check_sorted  
+
+
 
 check_right_side:
     inc %r11
     mov %rdi, %rsi # restore the current node into rsi
+    mov 12(%rsi), %rax 
+    mov %r9, %rbx
+    jmp check_sorted  
 
     
 
@@ -70,41 +76,47 @@ check_sorted:
     mov $1, %r13 # increasing flag = true
     mov $1, %r14 # decreasing flag = true
 
-// check if list is sorted, asume the start node = rax, end node = rbx
-check_sorted_loop:
     test %rax, %rax 
-    je not_sorted  # check if start is null
+    je is_sorted  # check if start is null
 
     test %rbx, %rbx 
-    je not_sorted  # check if end is null
+    je is_sorted  # check if end is null
 
     mov %rax, %rsi
-    mov 12(%rsi), %r15
-    cmp %rbx, 12(%rsi)             # Compare current->next (12 bytes offset) with end
+
+// check if list is sorted, asume the start node = rax, end node = rbx
+check_sorted_loop:
+
+    cmp %rbx, %rsi            # Compare current->next (12 bytes offset) with end
     je check_sorted_loop_end       # If current->next == end, jump to loop_end
 
-    mov 12(%rsi), %rdx #   rdx = current->next
-    mov 8(%rdx) ,%rcx  #   rcx = current->next->data
+    mov 12(%rsi), %rdx #   rdx = current->next  
+
+    movsxd 8(%rdx), %rcx  #   rcx = current->next->data
+
+    movsxd 8(%rsi), %rdx      # rax = (signed) current->data
+    
+    
 
     
-    cmp  %rcx ,8(%rsi) # check if current->next->data < current->data 
+    cmp  %rcx ,%rdx # check if current->next->data < current->data 
     jg set_not_increasing
 
-continue_loop1:
-    cmp  %rcx ,8(%rsi) # check if current->next->data > current->data
+continue_sorted_loop1:
+    cmp  %rcx ,%rdx # check if current->next->data > current->data
     jl set_not_decreasing
 
-continue_loop2:
+continue_sorted_loop2:
     mov 12(%rsi), %rsi     # Move to the next node (current = current->next)
     jmp check_sorted_loop         # Repeat the loop    
 
 set_not_increasing: 
     mov $0, %r13
-    jmp continue_loop1
+    jmp continue_sorted_loop1
 
 set_not_decreasing:
     mov $0, %r14
-    jmp continue_loop2
+    jmp continue_sorted_loop2
 
 check_sorted_loop_end:
     cmp $1 ,%r13 # check if increasing seria
@@ -113,20 +125,21 @@ check_sorted_loop_end:
     cmp $1 ,%r14  # check if decreasing seria
     je is_sorted
 
-    jmp not_sorted
+    jmp next_node
 
 is_sorted:
     cmp $0, %r11
     je check_right_side
     jmp set_sort_count
-
 set_sort_count:
-    inc %r10 # inc coount if the left and the right side is sorted
+    inc %r10        # inc coount if the left and the right side is sorted
 
-not_sorted:
-    mov %rdi, %rsi # restore the current node into rsi
+next_node:
+    jmp all_nodes_loop
 
-    jmp sort_check_for_all_nodes
+set_result:
+    movq %r10, result
+    jmp done
 
-Done:
-    mov %r10, result
+done: 
+
