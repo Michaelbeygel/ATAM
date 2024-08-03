a30cb88007c366f2a74cbdecd6156c95e19924b0
Aviv Avital <avivavital111@gmail.com>
2024-08-04 00:03:23 +0300

    Now jumping according to command length

--- HW3/part2/ili_handler.asm
+++ HW3/part2/ili_handler.asm
@@ -4,6 +4,9 @@
 .align 4, 0x90
 
 my_ili_handler:
+# using r15 as addition to stack pointer, backing it up first
+  pushq %r15
+# backing up rest of registers
   pushq %rax
   pushq %rbx
   pushq %rcx
@@ -18,13 +21,13 @@ my_ili_handler:
   pushq %r12
   pushq %r13
   pushq %r14
-  pushq %r15
   xor %rdi, %rdi
   xor %rbx, %rbx
   xor %rax, %rax
+  xor %r15, %r15
 
   # Calculate the correct offset to the saved RIP
-  movq 120(%rsp), %rbx       # Store current stack pointer in rbx
+  movq 120(%rsp), %rbx       # Store current stack pointer in rbx ( 15 registers of 8 bytes)
 
   # Retrieve the RIP from the stack
   movq (%rbx), %rbx     # RIP command address is now in RBX
@@ -33,6 +36,7 @@ my_ili_handler:
   cmp $0x0F, %rdi   # Check if command size is 2 bytes
   je two_bytes
   call what_to_do
+  movq $1, %r15
   jmp restore_registers
 
 two_bytes:
@@ -40,10 +44,10 @@ two_bytes:
   movb %bh, %al
   movq %rax, %rdi
   call what_to_do
+  movq $2, %r15
 
 restore_registers:
   movq %rax, %rdi # Restore the return value of what to do in RDI
-  popq %r15
   popq %r14
   popq %r13
   popq %r12
@@ -61,10 +65,12 @@ restore_registers:
 
   cmp $0x0, %rdi # check if return value of what to do is 0
   je old_ili
-  addq $2, (%rsp) 
+  addq %r15, 8(%rsp) # pointing rip to next command, rip is 8 higher than due to r15 in stack
+  popq %r15
   jmp end
 
 old_ili:
+  popq %r15
   jmp * old_ili_handler
 
 end:
